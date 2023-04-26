6d7c841600b41b80f2da61e6db0b9cfb3f53e1bd
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-26 15:37:37 -0500

    Template Engine

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
-
 fccTesting(app); //For FCC testing purposes
+
+app.set('view engine', 'pug')
+app.set('views', './views/pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+res.render('index')
 });
 
 const PORT = process.env.PORT || 3000;
