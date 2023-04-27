41a8e50b3ccbefdd669b2e4f6a759003235b1d07
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-27 13:04:45 -0500

    cookie_session

--- server.js
+++ server.js
@@ -3,13 +3,25 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session')
+const passport = require('passport')
+
 
 const app = express();
-fccTesting(app); //For FCC testing purposes
 
+
+fccTesting(app); //For FCC testing purposes
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie:{secure:false}
+}))
 app.set('view engine', 'pug')
 app.set('views', './views/pug')
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
