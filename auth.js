35b059861a59da1f0c4c568ba2d618a639cc280c
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-05-02 08:38:22 -0500

    update1.2

--- auth.js
+++ auth.js
@@ -9,7 +9,7 @@ module.exports = async(app, myDataBase) => {
   passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/github/callback"
+    callbackURL: "https://raziel-advancednode-production.up.railway.app/auth/github/callback"
   },
     (accessToken, refreshToken, profile, cb) => {
       //console.log(profile)
