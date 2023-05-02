c7397b955ba8bfb7fc61a361213e8538c22b81f9
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-05-02 16:29:16 -0500

    test.1.3

--- auth.js
+++ auth.js
@@ -19,7 +19,7 @@ module.exports = async(app, myDataBase) => {
   passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'http://localhost:8080/auth/github/callback'
+    callbackURL: 'http://127.0.0.1:8080/auth/github/callback'
   },
     (accessToken, refreshToken, profile, cb) => {
       console.log(profile)
